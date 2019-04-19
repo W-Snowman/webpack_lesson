963660e122f63db35d952535dee441dd9e7fcde5
地场卫 <weidaner1621@126.com>
2019-04-19 14:45:40 +0800

    第一次提交

---
+++ webpack_3.2/src/index.js
@@ -0,0 +1,7 @@
+import bg from './bg_img.jpg'
+
+var root = document.getElementById("root");
+var img = new Image();
+img.src = bg;
+root.append(img);
+
---
+++ webpack_3.2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [{
+            test: /\.(jpg|png|gif)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images', 
+                    limit: 20480
+                }
+            }]
+        }]
+    },
+    output: {
+        path: path.resolve(__dirname,"dist"),  
+        filename: 'bundle.js'
+    }
+}
+
